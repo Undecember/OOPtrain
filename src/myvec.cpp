57fcee871b5e40f05c1ceab1cef0e3c9fb2bab21
Undecember <ehdtjskaqhd@gmail.com>
2024-02-17 16:50:58 +0900

    Add TODOs

--- src/myvec.cpp
+++ src/myvec.cpp
@@ -6,54 +6,37 @@ using namespace myvec;
 #include <cstring>
 
 MyVector::MyVector() {
-    ptr = 0;
-    _size = capacity = 0;
+    // TODO
 }
 
 MyVector::~MyVector() {
-    if (ptr) free(ptr);
+    // TODO
 }
 
 void MyVector::push_back(int value) {
-    if (capacity <= _size) {
-        if (capacity) capacity <<= 1;
-        else capacity = 1;
-        ptr = (int*)realloc(ptr, capacity * sizeof(int));
-    }
-    ptr[_size++] = value;
+    // TODO
 }
 
 void MyVector::pop_back() {
-    _size--;
+    // TODO
 }
 
 void MyVector::clear() {
-    _size = 0;
+    // TODO
 }
 
 void MyVector::resize(unsigned int n) {
-    if (n == _size) return;
-    if (n < _size) {
-        _size = n;
-        return;
-    }
-    if (capacity <= n) {
-        while (capacity <= n) capacity <<= 1;
-        ptr = (int*)realloc(ptr, capacity * sizeof(int));
-    }
-    memset(ptr + _size, 0, (n - _size) * sizeof(int));
-    _size = n;
+    // TODO
 }
 
 unsigned int MyVector::size() {
-    return _size;
+    // TODO
 }
 
 bool MyVector::empty() {
-    return _size == 0;
+    // TODO
 }
 
 int MyVector::get(int i) {
-    if (i < 0 || i >= (int)_size) return 0;
-    return ptr[i];
+    // TODO
 }
